a3c8c539070b2a41f9702d23b8f30269c53a3f16
User <xnikug>
2025-04-18 10:52:46 +0300

    Added room movement

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from assets import get_cyberpunk_art
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    art = get_cyberpunk_art()
+    room = request.args.get("room", "home")
+    art = get_cyberpunk_art(room)
     return render_template("index.html", art=art)
 
 if __name__ == "__main__":
